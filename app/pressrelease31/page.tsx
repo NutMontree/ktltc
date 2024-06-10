a2869cfed0ffde7eaecbb1bea4261c4dac4e7f57
NutMontree <nutmontree29@gmail.com>
2024-06-10 11:45:26 +0700

    Update Pressrelease31

--- app/pressrelease31/page.tsx
+++ app/pressrelease31/page.tsx
@@ -9,7 +9,7 @@ import {
   Button,
   useDisclosure,
 } from "@nextui-org/react";
-import { DatatPressrelease30 } from "./data";
+import { DatatPressrelease31 } from "./data";
 import { Image } from "@nextui-org/react";
 import Link from "next/link";
 
@@ -31,14 +31,14 @@ export default function Pressrelease() {
               <>
                 <ModalHeader className="flex flex-col gap-1">
                   <div>
-                    {DatatPressrelease30.Item.map((item) => (
+                    {DatatPressrelease31.Item.map((item) => (
                       <div key={item.title}>{item.title}</div>
                     ))}
                   </div>
                 </ModalHeader>
                 <ModalBody>
                   <p>
-                    {DatatPressrelease30.Item.map((item) => (
+                    {DatatPressrelease31.Item.map((item) => (
                       <div key={item.description}>
                         <div>{item.description}</div>
                         <div className="text-xs text-slate-500">
@@ -49,7 +49,7 @@ export default function Pressrelease() {
                   </p>
                   <div></div>
                   <div className="justify-center flex-col">
-                    {DatatPressrelease30.Item.map((item) => (
+                    {DatatPressrelease31.Item.map((item) => (
                       <div
                         className="mb-4 group relative shadow-lg rounded-xl px-[144px] py-[144px]"
                         key={item.img1}
@@ -65,6 +65,46 @@ export default function Pressrelease() {
                           <Image src={item.img8} alt={""}></Image>
                           <Image src={item.img9} alt={""}></Image>
                           <Image src={item.img10} alt={""}></Image>
+                          <Image src={item.img11} alt={""}></Image>
+                          <Image src={item.img12} alt={""}></Image>
+                          <Image src={item.img13} alt={""}></Image>
+                          <Image src={item.img14} alt={""}></Image>
+                          <Image src={item.img15} alt={""}></Image>
+                          <Image src={item.img16} alt={""}></Image>
+                          <Image src={item.img17} alt={""}></Image>
+                          <Image src={item.img18} alt={""}></Image>
+                          <Image src={item.img19} alt={""}></Image>
+                          <Image src={item.img20} alt={""}></Image>
+                          <Image src={item.img21} alt={""}></Image>
+                          <Image src={item.img22} alt={""}></Image>
+                          <Image src={item.img23} alt={""}></Image>
+                          <Image src={item.img24} alt={""}></Image>
+                          <Image src={item.img25} alt={""}></Image>
+                          <Image src={item.img26} alt={""}></Image>
+                          <Image src={item.img27} alt={""}></Image>
+                          <Image src={item.img28} alt={""}></Image>
+                          <Image src={item.img29} alt={""}></Image>
+                          <Image src={item.img30} alt={""}></Image>
+                          <Image src={item.img31} alt={""}></Image>
+                          <Image src={item.img32} alt={""}></Image>
+                          <Image src={item.img33} alt={""}></Image>
+                          <Image src={item.img34} alt={""}></Image>
+                          <Image src={item.img35} alt={""}></Image>
+                          <Image src={item.img36} alt={""}></Image>
+                          <Image src={item.img37} alt={""}></Image>
+                          <Image src={item.img38} alt={""}></Image>
+                          <Image src={item.img39} alt={""}></Image>
+                          <Image src={item.img40} alt={""}></Image>
+                          <Image src={item.img41} alt={""}></Image>
+                          <Image src={item.img42} alt={""}></Image>
+                          <Image src={item.img43} alt={""}></Image>
+                          <Image src={item.img44} alt={""}></Image>
+                          <Image src={item.img45} alt={""}></Image>
+                          <Image src={item.img46} alt={""}></Image>
+                          <Image src={item.img47} alt={""}></Image>
+                          <Image src={item.img48} alt={""}></Image>
+                          <Image src={item.img49} alt={""}></Image>
+                          <Image src={item.img50} alt={""}></Image>
                         </div>
                       </div>
                     ))}
