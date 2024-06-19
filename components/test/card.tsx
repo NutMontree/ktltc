217b9b0cfd6c104336b614834f38cdddc39f9c3c
NutMontree <nutmontree29@gmail.com>
2024-06-19 13:18:51 +0700

    update CardPage

--- components/test/card.tsx
+++ components/test/card.tsx
@@ -13,7 +13,7 @@ export default function CardPage() {
             className="w-full h-[300px] col-span-12 sm:col-span-7"
           >
             <CardHeader className="absolute z-10 top-1 flex-col items-start">
-              <p className="text-tiny text-white/60 uppercase font-bold">
+              <div className="text-tiny text-white/60 uppercase font-bold">
                 Your day your way
               </div>
               <h4 className="text-white/90 font-medium text-xl">
@@ -34,8 +34,8 @@ export default function CardPage() {
                   src="https://nextui.org/images/breathing-app-icon.jpeg"
                 />
                 <div className="flex flex-col">
-                  <p className="text-tiny text-white/60">Breathing App</div>
-                  <p className="text-tiny text-white/60">
+                  <div className="text-tiny text-white/60">Breathing App</div>
+                  <div className="text-tiny text-white/60">
                     Get a good night's sleep.
                   </div>
                 </div>
@@ -51,7 +51,9 @@ export default function CardPage() {
             className="w-full h-[300px] col-span-12 sm:col-span-5"
           >
             <CardHeader className="absolute z-10 top-1 flex-col items-start">
-              <p className="text-tiny text-white/60 uppercase font-bold">New</div>
+              <div className="text-tiny text-white/60 uppercase font-bold">
+                New
+              </div>
               <h4 className="text-black font-medium text-2xl">Acme camera</h4>
             </CardHeader>
             <Image
@@ -62,8 +64,8 @@ export default function CardPage() {
             />
             <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
               <div>
-                <p className="text-black text-tiny">Available soon.</div>
-                <p className="text-black text-tiny">Get notified.</div>
+                <div className="text-black text-tiny">Available soon.</div>
+                <div className="text-black text-tiny">Get notified.</div>
               </div>
               <Button
                 className="text-tiny"
@@ -78,7 +80,7 @@ export default function CardPage() {
 
           <Card className="col-span-12 sm:col-span-4 h-[300px]">
             <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-              <p className="text-tiny text-white/60 uppercase font-bold">
+              <div className="text-tiny text-white/60 uppercase font-bold">
                 What to watch
               </div>
               <h4 className="text-white font-medium text-large">
@@ -95,7 +97,7 @@ export default function CardPage() {
 
           <Card className="col-span-12 sm:col-span-4 h-[300px]">
             <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-              <p className="text-tiny text-white/60 uppercase font-bold">
+              <div className="text-tiny text-white/60 uppercase font-bold">
                 Plant a tree
               </div>
               <h4 className="text-white font-medium text-large">
@@ -112,7 +114,7 @@ export default function CardPage() {
 
           <Card className="col-span-12 sm:col-span-4 h-[300px]">
             <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-              <p className="text-tiny text-white/60 uppercase font-bold">
+              <div className="text-tiny text-white/60 uppercase font-bold">
                 Supercharged
               </div>
               <h4 className="text-white font-medium text-large">
