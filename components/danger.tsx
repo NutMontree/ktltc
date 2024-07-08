9ff6ecbf1566bb7152bdabd66ec11feca03b13b4
NutMontree <nutmontree29@gmail.com>
2024-07-08 11:51:37 +0700

    v2

--- components/danger.tsx
+++ components/danger.tsx
@@ -2,7 +2,7 @@ export default function Danger() {
   return (
     <>
       <div className="text-center bg-red-500">
-        กำลังอัพเดตข้อมูล ช่วงเดือน เมาษายน 2567
+        กำลังอัพเดตข้อมูล ช่วงเดือน พฤษภาคม 2567
       </div>
     </>
   );
