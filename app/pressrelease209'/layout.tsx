30e9bbd41e8dc917468c88e17fe3507b60c7d38b
NutMontree <nutmontree29@gmail.com>
2024-07-10 16:21:49 +0700

    v2

---
+++ app/pressrelease209'/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <section>
+      <div className="px-8 py-8">{children}</div>
+    </section>
+  );
+}
