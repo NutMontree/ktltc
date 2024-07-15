f2d26334c68e668c2ff91822b4fc82847f0c5e32
NutMontree <nutmontree29@gmail.com>
2024-07-15 13:43:45 +0700

    v2

---
+++ app/pressrelease6707/data.ts
@@ -0,0 +1,13 @@
+export const DataPressrelease6707 = {
+  navItems: [
+    {
+      name: ` `,
+      // backgroundImage: "/images/ข่าวประชาสัมพันธ์/2567/กรกฏาคม/1/01.webp",
+      backgroundImage: " ",
+      description: `
+ ...`,
+      // href: "/pressrelease272",
+      href: " ",
+    },
+  ],
+};
