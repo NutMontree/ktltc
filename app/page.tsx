a95aefb7f0f3e3775b04423b0002f322bbd14d2e
NutMontree <nutmontree29@gmail.com>
2024-08-02 12:08:23 +0700

    v2

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ export default function page() {
               <Image
                 removeWrapper
                 alt="Card example background"
-                src="/images/จดหมายข่าว/2567/กรกฎาคม/34/1.webp"
+                src="/images/จดหมายข่าว/2567/สิงหาคม/1/0.webp"
               />
               <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
                 <div className="flex flex-grow gap-2 items-center">
