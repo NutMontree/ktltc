4787f132aae8f68e108ce8d2606cbc07edf25371
NutMontree <nutmontree29@gmail.com>
2024-08-06 10:21:22 +0700

    v2

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function page() {
                 removeWrapper
                 alt="Relaxing app background"
                 className="z-0 w-full h-full object-cover absolute "
-                src="/images/ข่าวประชาสัมพันธ์/2567/สิงหาคม/8/2.webp"
+                src="/images/ข่าวประชาสัมพันธ์/2567/สิงหาคม/7/02.webp"
               />
               <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
                 <div className="flex flex-grow gap-2 items-center">
@@ -67,7 +67,7 @@ export default function page() {
               <Image
                 removeWrapper
                 alt="Card example background"
-                src="/images/ข่าวประชาสัมพันธ์/2567/สิงหาคม/7/02.webp"
+                src="/images/จดหมายข่าว/2567/สิงหาคม/5/0.webp"
               />
               <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
                 <div className="flex flex-grow gap-2 items-center">
