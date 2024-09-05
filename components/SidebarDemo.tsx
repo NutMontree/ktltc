c88be7a64c8a4d8aa98470dc9f89a9d555849bbf
NutMontree <nutmontree29@gmail.com>
2024-09-05 16:48:59 +0700

    test

--- components/SidebarDemo.tsx
+++ components/SidebarDemo.tsx
@@ -13,7 +13,7 @@ import { motion } from "framer-motion";
 import { Image } from "@nextui-org/react";
 import { cn } from "@/lib/utils";
 
-export function SidebarDemo({ children }: { children: React.ReactNode }) {
+export function SidebarDemo() {
   const links = [
     {
       label: "Dashboard",
@@ -81,8 +81,7 @@ export function SidebarDemo({ children }: { children: React.ReactNode }) {
           </div>
         </SidebarBody>
       </Sidebar>
-      {/* <Dashboard children={undefined} /> */}
-      {children}
+      <Dashboard />
     </div>
   );
 }
@@ -117,14 +116,25 @@ export const LogoIcon = () => {
 // Dummy dashboard component with content
 const Dashboard = () => {
   return (
-    <>
-      <div className="flex flex-1">
-        <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-          <div className=" ">
-            <div>Dashboard</div>
-          </div>
+    <div className="flex flex-1">
+      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+        <div className="flex gap-2">
+          {[...new Array(4)].map((i) => (
+            <div
+              key={"first-array" + i}
+              className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
+            ></div>
+          ))}
+        </div>
+        <div className="flex gap-2 flex-1">
+          {[...new Array(2)].map((i) => (
+            <div
+              key={"second-array" + i}
+              className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
+            ></div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
