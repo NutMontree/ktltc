9fe98917e75866265f6a14ea166ba8d89c71c3a2
NutMontree <nutmontree29@gmail.com>
2024-10-01 09:30:44 +0700

    อ/

--- app/announcement6709/data.ts
+++ app/announcement6709/data.ts
@@ -2,6 +2,14 @@ export const DataAnnouncement = {
   navItems: [
     {
       name: `ประกาศ `,
+      backgroundImage: "/images/ข่าวประชาสัมพันธ์/2567/กันยายน/56/2.webp",
+      description: `
+กำหนดการลงทะเบียนเรียน เเละค่าลงทะเบียนเรียน ภาคเรียนที่ ๒/๒๕๖๗ 
+      ...`,
+      href: "/pressrelease409",
+    },
+    {
+      name: `ประกาศ `,
       backgroundImage: "/images/ข่าวประชาสัมพันธ์/2567/กันยายน/52/1.webp",
       description: `
 รายชื่อผลการสอบคัดเลือก ลูกจ้างชั่วคราว ตำเเหน่งครูพิเศษสอน สาขาวิชาการจัดการโลจิสติกส์ 
