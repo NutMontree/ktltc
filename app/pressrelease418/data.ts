e17990eb0d6b7313e11fa74851379b752e8c1478
NutMontree <nutmontree29@gmail.com>
2024-10-08 07:50:54 +0700

    v2

--- app/pressrelease418/data.ts
+++ app/pressrelease418/data.ts
@@ -31,7 +31,7 @@ export const Description = [
   },
 ];
 
-export const DataDate = [{ date: `4 ตุลาคม 2567` }];
+export const DataDate = [{ date: `7 ตุลาคม 2567` }];
 
 export const ImageItem = [
   { img: "/images/ข่าวประชาสัมพันธ์/2567/ตุลาคม/8/01.webp" },
