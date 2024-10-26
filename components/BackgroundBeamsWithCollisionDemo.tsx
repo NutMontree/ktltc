fac4de2fb0c869460d7cd32e70ee1fadd0333021
NutMontree <nutmontree29@gmail.com>
2024-10-26 12:31:53 +0700

    v3

--- components/BackgroundBeamsWithCollisionDemo.tsx
+++ components/BackgroundBeamsWithCollisionDemo.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export function BackgroundBeamsWithCollisionDemo() {
   return (
     <>
-      {/* <Link href="pressrelease409">
+      {/* <Link href="pressrelease450">
         <BackgroundBeamsWithCollision>
           <div className="scale-95 hover:scale-110 duration-500 justify-center">
             <Image
@@ -17,31 +17,29 @@ export function BackgroundBeamsWithCollisionDemo() {
           </div>
         </BackgroundBeamsWithCollision>
       </Link> */}
-
-      <div className="flex justify-center px-8">
-        <div>
-          <div className="flex justify-center">
-            <Image
-              className="h-[300px] sm:h-[500px] lg:h-[700px] xl:h-[900px] "
-              src={"images/ข่าวประชาสัมพันธ์/2567/ตุลาคม/39/1.webp"}
-            />
-          </div>
-
-          <br />
+      <Link href="pressrelease450">
+        <div className="flex justify-center px-8">
           <div>
-            <p className="flex justify-center text-base">" วันปิยมหาราช "</p>
-            <p className="flex justify-center text-base">
-              ๒๓ ตุลาคม ๒๕๖๗ " วันปิยมหาราช "
-              พระบาทสมเด็จพระจุลจอมเกล้าเจ้าอยู่หัว ปวงข้าพระพุทธเจ้า
-              น้อมรำลึกในพระมหากรุณาธิคุณ
-            </p>
-            <p className="flex justify-center text-base">
-              👉ด้วยเกล้าด้วยกระหม่อมขอเดชะ ข้าพระพุทธเจ้า คณะผู้บริหาร ครู
-              บุคลากรทางการศึกษา เเละนักเรียน นักศึกษา วิทยาลัยเทคนิคกันทรลักษ์
-            </p>
+            <div className="flex justify-center">
+              <Image
+                className="h-[300px] sm:h-[500px] lg:h-[700px] xl:h-[900px] "
+                src={"images/ข่าวประชาสัมพันธ์/2567/ตุลาคม/44/1.webp"}
+              />
+            </div>
+
+            <br />
+
+            <div>
+              <p className="flex justify-center text-base">" ประกาศรายชื่อ "</p>
+              <p className="flex justify-center text-base">
+                ผู้มีสิทธิเข้ารับการประเมินสมรรถนะ ตำเเหน่ง ครูพิเศษสอน
+                สาขาวิชาช่างไฟฟ้ากำลัง เข้ารับการประเมินสมรรถนะ ในวันที่ 29
+                ตุลาคม 2567 ตามวันเวลาที่กำหนด
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 }
