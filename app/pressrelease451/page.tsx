9345a840f08b61257c1e4d13ca2765acd375b89e
NutMontree <nutmontree29@gmail.com>
2024-10-26 12:47:30 +0700

    v3

--- app/pressrelease451/page.tsx
+++ app/pressrelease451/page.tsx
@@ -90,7 +90,7 @@ export default function Pressrelease() {
         <br />
 
         <div className="flex justify-center">
-          <div className="  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center justify-center ">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center justify-center ">
             {ImageItem.map((item) => (
               <div className="" key={item.img}>
                 <div className="scale-90 hover:scale-110 transition duration-500 rounded-full">
