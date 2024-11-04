f1a39448552a22e1d7f0a8f305a3f33a1094ba7f
NutMontree <nutmontree29@gmail.com>
2024-11-04 11:22:13 +0700

    v3

---
+++ app/pressrelease470/data.ts
@@ -0,0 +1,56 @@
+export const DataPressrelease = {
+  Item: [
+    {
+      title: `
+ขอเชิญชวนผู้ปกครองทุกท่าน
+      `,
+    },
+  ],
+};
+
+export const Description = [
+  {
+    description: `
+ร่วมเลือก(โหวต)วันประชุมผู้ปกครอง
+วิทยาลัยเทคนิคกันทรลักษ์ ภาคเรียนที่ 2 ปีการศึกษา 2567 โดยมีให้เลือกทั้งหมด 2 วัน ได้เเก่
+    `,
+  },
+  {
+    description: `
+👉 วันเสาร์ที่ 9 พศจิกายน 2567
+    `,
+  },
+  {
+    description: `
+👉 วันเสาร์ที่ 14 ธันวาคม 2567
+    `,
+  },
+  {
+    description: `
+
+    `,
+  },
+  {
+    description: `
+
+    `,
+  },
+  {
+    description: `
+
+    `,
+  },
+  {
+    description: `
+
+    `,
+  },
+];
+
+export const DataDate = [{ date: `4 ตุลาคม 2567` }];
+
+export const ImageItem = [
+  { img: "/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/7/1.webp" },
+
+  // { img: "/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/4/0.webp" },
+];
