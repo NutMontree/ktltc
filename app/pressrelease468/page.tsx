8ff52aa14031abea3f63cbcc8e4a8f56fc070a78
NutMontree <nutmontree29@gmail.com>
2024-11-04 13:05:41 +0700

    v3

--- app/pressrelease468/page.tsx
+++ app/pressrelease468/page.tsx
@@ -106,7 +106,8 @@ export default function Pressrelease() {
           <div className="px-2 py-2  gap-4">
             <iframe
               className="h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 
-                         w-[300px] sm:w-[600px] lg:w-[700px] xl:w-[1080px] "
+                         w-[300px] sm:w-[600px] lg:w-[700px] xl:w-[1080px] 
+                         rounded-lg"
               src="https://www.youtube.com/embed/1qwOVzMyCQU?si=JlO30KsNt-hv68Ls"
               title="เข้าค่ายลูกเสือ ปี 2567 part 1"
               // frameborder="0"
