c0acdbaaebd6b75a7134bcd70c9afe36c0ec26eb
NutMontree <nutmontree29@gmail.com>
2024-11-13 13:18:30 +0700

    v3

--- components/BackgroundBeamsWithCollisionDemo.tsx
+++ components/BackgroundBeamsWithCollisionDemo.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 import { Image } from "@nextui-org/react";
 import Link from "next/link";
+import { Cover } from "@/components/ui/cover";
 
 export function BackgroundBeamsWithCollisionDemo() {
   return (
@@ -18,147 +18,149 @@ export function BackgroundBeamsWithCollisionDemo() {
         </BackgroundBeamsWithCollision>
       </Link> */}
 
-      <Link href="pressrelease471">
-        <div className="flex justify-center px-8">
-          <div>
-            <div className="flex justify-center">
-              <Image
-                // className="h-[300px] sm:h-[500px] lg:h-[700px] xl:h-[900px] "
-                className="h-[150px] sm:h-[280px] lg:h-[400px] xl:h-[600px] "
-                src={"/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/10/1.webp"}
-              />
-            </div>
-            <br />
-            <div className="text-center">
-              💥สวัสดี Dek68 ทุกคน วิทยาลัยเทคนิคกันทรลักษ์ เปิดรับสมัครนักเรียน{" "}
+      <Cover className="flex justify-center">
+        <Link href="pressrelease471">
+          <div className="flex justify-center px-8">
+            <div>
+              <div className="flex justify-center">
+                <Image
+                  // className="h-[300px] sm:h-[500px] lg:h-[700px] xl:h-[900px] "
+                  className="h-[150px] sm:h-[280px] lg:h-[400px] xl:h-[600px] "
+                  src={"/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/10/1.webp"}
+                />
+              </div>
               <br />
-              นักศึกษา ปีการศึกษา 2568 📌📍รู้หรือไม่? ว่า <br />
-              วิทยาลัยเทคนิคกันทรลักษ์ ทำการเปิดสาขาวิชาเพิ่ม ได้แก่ 👉ระดับ{" "}
-              <br />
-              ปวช. สาขาวิชาเมคคาทรอนิกส์เเละหุ่นยนต์ สาขาวิชาโยธา <br />
-              สาขาวิชาตัวถังเเละสีรถยนต์ 👉ระดับ ปวส. <br />
-              สาขาวิชาเมคคาทรอนิกส์เเละหุ่นยนต์ สาขาวิชาโยธา <br />
-              สาขาวิชาเทคโนโลยีอุตสาหกรรมตัวถังเเละสีรถยนต์ <br />
-              สาขาวิชาการจัดการโลจิสติกส์เเละซัพพลายเชน <br />
-              📣📣นอกจากนี้ยังมีสาขาวิชาอีกมากมาย อย่าลืมมาเจอกันนะค้าบ <br />
-              พวกเราหัวใจ แสด – น้ำเงิน ยินดีต้อนรับนักเรียน - นักศึกษา เด็ก 68
-              ทุกคนค้าบ
+              <div className="text-center">
+                💥สวัสดี Dek68 ทุกคน วิทยาลัยเทคนิคกันทรลักษ์
+                เปิดรับสมัครนักเรียน <br />
+                นักศึกษา ปีการศึกษา 2568 📌📍รู้หรือไม่? ว่า <br />
+                วิทยาลัยเทคนิคกันทรลักษ์ ทำการเปิดสาขาวิชาเพิ่ม ได้แก่ 👉ระดับ
+                <br />
+                ปวช. สาขาวิชาเมคคาทรอนิกส์เเละหุ่นยนต์ สาขาวิชาโยธา <br />
+                สาขาวิชาตัวถังเเละสีรถยนต์ 👉ระดับ ปวส. <br />
+                สาขาวิชาเมคคาทรอนิกส์เเละหุ่นยนต์ สาขาวิชาโยธา <br />
+                สาขาวิชาเทคโนโลยีอุตสาหกรรมตัวถังเเละสีรถยนต์ <br />
+                สาขาวิชาการจัดการโลจิสติกส์เเละซัพพลายเชน <br />
+                📣📣นอกจากนี้ยังมีสาขาวิชาอีกมากมาย อย่าลืมมาเจอกันนะค้าบ <br />
+                พวกเราหัวใจ แสด – น้ำเงิน ยินดีต้อนรับนักเรียน - นักศึกษา เด็ก
+                68 ทุกคนค้าบ
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
-      <br />
-      <br />
-      <Link href="pressrelease471">
-        <div className="flex justify-center px-8">
-          <div>
-            <div className="flex justify-center">
-              <Image
-                // className="h-[300px] sm:h-[500px] lg:h-[700px] xl:h-[900px] "
-                className="h-[250px] sm:h-[500px] lg:h-[600px] xl:h-[600px] "
-                src={"/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/8/1.webp"}
-              />
-            </div>
+        </Link>
+      </Cover>
 
-            <br />
-
+      <div className="pt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 justify-items-center">
+        <Cover>
+          <Link href="pressrelease471">
             <div>
-              <p className="flex justify-center text-base">
-                " Dek 68 พร้อมกันรึยัง "
-              </p>
-              <p className="flex justify-center text-base">
-                ประชาสัมพันธ์ วิทยาลัยเทคนิคกันทรลักษ์ เปิดรับสมัคร
-              </p>
-              <p className="flex justify-center text-base">
-                นักเรียนนักศึกษาใหม่ รอบโควตา ปีการศึกษา 2568
-              </p>
-              <p className="flex justify-center text-base">
-                💥💥เปิดรับสมัครตั้งเเต่วันนี้ ถึง 21 พฤศจิกายน 2567
-              </p>
+              <div>
+                <div className="flex justify-center">
+                  <Image
+                    className="h-[200px] sm:h-[300px] lg:h-[400px] xl:h-[400px] "
+                    src={"/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/8/1.webp"}
+                  />
+                </div>
+
+                <br />
+
+                <div className="text-base text-center text-[10px] md:text-[12px] pr-1 sm:text-sm md:text-base lg:text-base ">
+                  " Dek 68 พร้อมกันรึยัง "<br />
+                  ประชาสัมพันธ์ วิทยาลัยเทคนิคกันทรลักษ์ เปิดรับสมัคร <br />
+                  นักเรียนนักศึกษาใหม่ รอบโควตา ปีการศึกษา 2568
+                  <br />
+                  💥💥เปิดรับสมัครตั้งเเต่วันนี้ ถึง 21 พฤศจิกายน 2567
+                  <br />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </Link>
-      <br />
-      <br />
-      <Link href="pressrelease479">
-        <div className="flex justify-center px-8">
-          <div>
+          </Link>
+        </Cover>
+
+        <Cover>
+          <Link href="pressrelease479">
             <div className="flex justify-center">
-              <Image
-                // className="h-[300px] sm:h-[500px] lg:h-[700px] xl:h-[900px] "
-                className="h-[250px] sm:h-[500px] lg:h-[600px] xl:h-[600px] "
-                src={"/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/16/1.webp"}
-              />
-            </div>
+              <div>
+                <div className="flex justify-center">
+                  <Image
+                    // className="h-[300px] sm:h-[500px] lg:h-[700px] xl:h-[900px] "
+                    className="h-[200px] sm:h-[300px] lg:h-[400px] xl:h-[400px] "
+                    src={"/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/16/1.webp"}
+                  />
+                </div>
 
-            <br />
+                <br />
 
-            <div>
-              <p className="flex justify-center text-base">
-                เเจ้ง 💥💥 ถึงนักเรียน นักศึกษา วิทยาลัยเทคนิคกันทรลักษ์
-              </p>
-              <p className="flex justify-center text-base">
-                เจ้าหน้าที่งานปกครองจะเริ่มออกตรวจบริเวณรอบนอกวิทยาลัยฯ ในเวลา
-                12.00 น.
-              </p>
-              <p className="flex justify-center text-base">
-                ให้นักเรียน นักศึกษาทุกคนทำตามกฎระเบียบ
-                เเละอยู่บริเวณภายในวิทยาลัยฯ
-              </p>
-              <p className="flex justify-center text-base">
-                หากมีพฤติกรรมดังนี้ ให้รีบปรับปรุงตนเองด้วยนะคะ/ครับ
-              </p>
+                <div>
+                  <p className="flex justify-center text-base text-center text-[10px] md:text-[12px] pr-1 sm:text-sm md:text-base lg:text-base ">
+                    เเจ้ง 💥💥 ถึงนักเรียน นักศึกษา วิทยาลัยเทคนิคกันทรลักษ์
+                  </p>
+                  <p className="flex justify-center text-base text-center text-[10px] md:text-[12px] pr-1 sm:text-sm md:text-base lg:text-base ">
+                    เจ้าหน้าที่งานปกครองจะเริ่มออกตรวจบริเวณรอบนอกวิทยาลัยฯ
+                    ในเวลา 12.00 น.
+                  </p>
+                  <p className="flex justify-center text-base text-center text-[10px] md:text-[12px] pr-1 sm:text-sm md:text-base lg:text-base ">
+                    ให้นักเรียน นักศึกษาทุกคนทำตามกฎระเบียบ
+                    เเละอยู่บริเวณภายในวิทยาลัยฯ
+                  </p>
+                  <p className="flex justify-center text-base text-center text-[10px] md:text-[12px] pr-1 sm:text-sm md:text-base lg:text-base ">
+                    หากมีพฤติกรรมดังนี้ ให้รีบปรับปรุงตนเองด้วยนะคะ/ครับ
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </Link>
-      <br />
-      <br />
-      <Link href="pressrelease479">
-        <div className="flex justify-center px-8">
-          <div>
+          </Link>
+        </Cover>
+
+        <Cover>
+          <Link href="pressrelease479">
             <div className="flex justify-center">
-              <Image
-                // className="h-[300px] sm:h-[500px] lg:h-[700px] xl:h-[900px] "
-                className="h-[250px] sm:h-[500px] lg:h-[600px] xl:h-[600px] "
-                src={"/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/19/1.webp"}
-              />
-            </div>
+              <div>
+                <div className="flex justify-center">
+                  <Image
+                    // className="h-[300px] sm:h-[500px] lg:h-[700px] xl:h-[900px] "
+                    className="h-[200px] sm:h-[300px] lg:h-[400px] xl:h-[400px] "
+                    src={"/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/19/1.webp"}
+                  />
+                </div>
 
-            <br />
+                <br />
 
-            <div>
-              <p className="flex justify-center text-base">
-                วันอังคารที่ 12 พฤศจิกายน 2567 วิทยาลัยเทคนิคกันทรลักษ์
-              </p>
-              {/* ***************************** ลบออก ***************************** */}
-              <div className="">
-                <p>
-                  แจ้ง ‼️ผลการเรียน ของนักเรียน นักศึกษา ภาคเรียนที่ 1
-                  ปีการศึกษา 2567
-                </p>
-                <p className="text-sky-800 text-lg text-center ">
-                  สามารถดูได้ผ่านระบบลิ้งค์
-                </p>
-                <div className=" flex justify-center ">
-                  <Link
-                    href="
+                <div>
+                  <p className="flex justify-center text-base text-center text-[10px] md:text-[12px] pr-1 sm:text-sm md:text-base lg:text-base ">
+                    วันอังคารที่ 12 พฤศจิกายน 2567 วิทยาลัยเทคนิคกันทรลักษ์
+                  </p>
+                  {/* ***************************** ลบออก ***************************** */}
+                  <div className="">
+                    <p className=" text-center text-[10px] md:text-[12px] pr-1 sm:text-sm md:text-base lg:text-base ">
+                      แจ้ง‼️ผลการเรียน ของนักเรียน นักศึกษา ภาคเรียนที่ 1
+                      ปีการศึกษา 2567
+                    </p>
+                    <p className="text-sky-800 text-lg text-center  text-center text-[10px] md:text-[12px] pr-1 sm:text-sm md:text-base lg:text-base ">
+                      สามารถดูได้ผ่านระบบลิ้งค์
+                    </p>
+                    <div className=" flex justify-center ">
+                      <Link
+                        href="
                     https://std2018.vec.go.th/web/
                     "
-                  >
-                    <p className="hover:text-sky-500 text-center ">
-                      https://std2018.vec.go.th/web/
+                      >
+                        <p className="hover:text-sky-500 text-center text-center text-[10px] md:text-[12px] pr-1 sm:text-sm md:text-base lg:text-base ">
+                          https://std2018.vec.go.th/web/
+                        </p>
+                      </Link>
+                    </div>
+                    <p className="flex justify-center text-center text-[10px] md:text-[12px] pr-1 sm:text-sm md:text-base lg:text-base ">
+                      หรือสแกน qr code{" "}
                     </p>
-                  </Link>
+                  </div>
+                  {/* ***************************** ลบออก ***************************** */}
                 </div>
-                <p className="flex justify-center">หรือสแกน qr code </p>
               </div>
-              {/* ***************************** ลบออก ***************************** */}
             </div>
-          </div>
-        </div>
-      </Link>
+          </Link>
+        </Cover>
+      </div>
     </>
   );
 }
