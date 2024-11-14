944d1f5244951405e6fe6f85e5dbf45c6c286954
NutMontree <nutmontree29@gmail.com>
2024-11-14 18:01:42 +0700

    v3

--- components/BackgroundBeamsWithCollisionDemo.tsx
+++ components/BackgroundBeamsWithCollisionDemo.tsx
@@ -2,64 +2,54 @@ import React from "react";
 import { Image } from "@nextui-org/react";
 import Link from "next/link";
 import { Cover } from "@/components/ui/cover";
-import { IconMathPiDivide2 } from "@tabler/icons-react";
 
 export function BackgroundBeamsWithCollisionDemo() {
   return (
     <>
-      {/* <Link href="pressrelease450">
-        <BackgroundBeamsWithCollision>
-          <div className="scale-95 hover:scale-110 duration-500 justify-center">
-            <Image
-              className="h-[300px] sm:h-[500px] lg:h-[700px] xl:h-[900px]"
-              src={"/images/ข่าวประชาสัมพันธ์/2567/กันยายน/56/2.webp"}
-              alt={""}
-            />
-          </div>
-        </BackgroundBeamsWithCollision>
-      </Link> */}
+      <div className="px-4 py-2">
+        <Cover className="flex justify-center">
+          <Link href="pressrelease471">
+            <div className="flex justify-center">
+              <div>
+                <div className="flex justify-center">
+                  <Image
+                    className="h-[150px] sm:h-[280px] lg:h-[400px] xl:h-[600px]"
+                    src={"/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/10/1.webp"}
+                  />
+                </div>
 
-      <Cover className="flex justify-center">
-        <Link href="pressrelease471">
-          <div className="flex justify-center px-8">
-            <div>
-              <div className="flex justify-center">
-                <Image
-                  // className="h-[300px] sm:h-[500px] lg:h-[700px] xl:h-[900px] "
-                  className="h-[150px] sm:h-[280px] lg:h-[400px] xl:h-[600px] "
-                  src={"/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/10/1.webp"}
-                />
-              </div>
-              <br />
-              <div className="text-base text-[10px] md:text-[12px] sm:text-sm ">
-                <div className="text-center">💥สวัสดี Dek68 ทุกคน</div>
-                วิทยาลัยเทคนิคกันทรลักษ์ เปิดรับสมัครนักเรียน นักศึกษา
-                ปีการศึกษา 2568 📌📍รู้หรือไม่? ว่า วิทยาลัยเทคนิคกันทรลักษ์
-                ทำการเปิดสาขาวิชาเพิ่ม ได้แก่
-                <div>
-                  👉ระดับ ปวช. สาขาวิชาเมคคาทรอนิกส์เเละหุ่นยนต์ สาขาวิชาโยธา
+                <div className="text-[10px] md:text-[12px] sm:text-sm md:text-base lg:text-base">
+                  <div className="text-center">💥สวัสดี Dek68 ทุกคน</div>
+                  วิทยาลัยเทคนิคกันทรลักษ์ เปิดรับสมัครนักเรียน นักศึกษา
+                  <br />
+                  ปีการศึกษา 2568 📌📍รู้หรือไม่? ว่า <br />
+                  วิทยาลัยเทคนิคกันทรลักษ์ ทำการเปิดสาขาวิชาเพิ่ม ได้แก่ 👉
+                  ระดับ ปวช.
+                  <br /> สาขาวิชาเมคคาทรอนิกส์เเละหุ่นยนต์ สาขาวิชาโยธา
                   สาขาวิชาตัวถังเเละสีรถยนต์
-                </div>
-                <div>
-                  👉ระดับ ปวส. สาขาวิชาเมคคาทรอนิกส์เเละหุ่นยนต์ สาขาวิชาโยธา
+                  <br /> 👉 ระดับ ปวส.
+                  <br />
+                  สาขาวิชาเมคคาทรอนิกส์เเละหุ่นยนต์ สาขาวิชาโยธา
+                  <br />
                   สาขาวิชาเทคโนโลยีอุตสาหกรรมตัวถังเเละสีรถยนต์
+                  <br />
                   สาขาวิชาการจัดการโลจิสติกส์เเละซัพพลายเชน
-                </div>
-                <div>📣📣นอกจากนี้ยังมีสาขาวิชาอีกมากมาย</div>
-                <div>
-                  อย่าลืมมาเจอกันนะค้าบ พวกเราหัวใจ แสด – น้ำเงิน
+                  <br />
+                  📣📣 น.อกจากนี้ยังมีสาขาวิชาอีกมากมาย อย่าลืมมาเจอกันนะค้าบ
+                  พวกเราหัวใจ แสด – น้ำเงิน
+                  <br />
                   ยินดีต้อนรับนักเรียน - นักศึกษา เด็ก 68 ทุกคนค้าบ
                 </div>
               </div>
             </div>
-          </div>
-        </Link>
-      </Cover>
+          </Link>
+        </Cover>
+      </div>
 
-      <div className="px-2 py-2 pt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 justify-items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 pt-6 px-4">
         <Cover>
           <Link href="pressrelease471">
-            <div>
+            <div className="flex justify-center">
               <div>
                 <div className="flex justify-center">
                   <Image
@@ -68,14 +58,12 @@ export function BackgroundBeamsWithCollisionDemo() {
                   />
                 </div>
 
-                <br />
-
-                <div className="text-base text-center text-[10px] md:text-[12px] sm:text-sm md:text-base lg:text-base ">
-                  " Dek 68 พร้อมกันรึยัง "<br />
-                  ประชาสัมพันธ์ วิทยาลัยเทคนิคกันทรลักษ์ เปิดรับสมัคร <br />
+                <div className="text-[10px] md:text-[12px] sm:text-sm md:text-base lg:text-base pt-4">
+                  " Dek 68 พร้อมกันรึยัง " ประชาสัมพันธ์
+                  วิทยาลัยเทคนิคกันทรลักษ์ เปิดรับสมัคร <br />
                   นักเรียนนักศึกษาใหม่ รอบโควตา ปีการศึกษา 2568
                   <br />
-                  💥💥เปิดรับสมัครตั้งเเต่วันนี้ ถึง 21 พฤศจิกายน 2567
+                  💥 เปิดรับสมัครตั้งเเต่วันนี้ ถึง 21 พฤศจิกายน 2567
                   <br />
                 </div>
               </div>
@@ -89,15 +77,11 @@ export function BackgroundBeamsWithCollisionDemo() {
               <div>
                 <div className="flex justify-center">
                   <Image
-                    // className="h-[300px] sm:h-[500px] lg:h-[700px] xl:h-[900px] "
                     className="h-[200px] sm:h-[300px] lg:h-[400px] xl:h-[400px] "
                     src={"/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/16/1.webp"}
                   />
                 </div>
-
-                <br />
-
-                <div className="flex justify-center text-base text-center text-[10px] md:text-[12px] sm:text-sm md:text-base lg:text-base ">
+                <div className="text-[10px] md:text-[12px] sm:text-sm md:text-base lg:text-base pt-4">
                   เเจ้ง 💥💥 ถึงนักเรียน นักศึกษา วิทยาลัยเทคนิคกันทรลักษ์
                   <br /> เจ้าหน้าที่งานปกครองจะเริ่มออกตรวจบริเวณรอบนอกวิทยาลัยฯ
                   ในเวลา 12.00 น.
@@ -123,14 +107,10 @@ export function BackgroundBeamsWithCollisionDemo() {
                     src={"/images/ข่าวประชาสัมพันธ์/2567/พฤศจิกายน/19/1.webp"}
                   />
                 </div>
-
-                <br />
-
                 <div>
-                  <div className="flex justify-center text-base text-center text-[10px] md:text-[12px] sm:text-sm md:text-base lg:text-base ">
+                  <div className="flex justify-center text-base text-center text-[10px] md:text-[12px] sm:text-sm md:text-base lg:text-base pt-4">
                     วันอังคารที่ 12 พฤศจิกายน 2567 วิทยาลัยเทคนิคกันทรลักษ์
                   </div>
-                  {/* ***************************** ลบออก ***************************** */}
                   <div className="">
                     <div className=" text-center text-[10px] md:text-[12px] sm:text-sm md:text-base lg:text-base ">
                       แจ้ง‼️ผลการเรียน ของนักเรียน นักศึกษา ภาคเรียนที่ 1
@@ -154,7 +134,6 @@ export function BackgroundBeamsWithCollisionDemo() {
                       หรือสแกน qr code
                     </div>
                   </div>
-                  {/* ***************************** ลบออก ***************************** */}
                 </div>
               </div>
             </div>
