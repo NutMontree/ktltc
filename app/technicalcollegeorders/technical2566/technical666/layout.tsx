f41ae6b5a3f7dab98a13601373cec66cfd062f0b
NutMontree <nutmontree29@gmail.com>
2024-11-21 08:42:28 +0700

    V3

--- app/technicalcollegeorders/technical2566/technical666/layout.tsx
+++
@@ -1,9 +0,0 @@
-// newsletter
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <section>
-      <div className="px-8 py-8">{children}</div>
-    </section>
-  );
-}
-
