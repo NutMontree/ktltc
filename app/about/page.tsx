330caae2b742a27375a46524d01f395121584ba6
NutMontree <nutmontree29@gmail.com>
2024-11-25 15:45:07 +0700

    v3.1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ import { title } from "@/components/primitives";
 export default function AboutPage() {
   return (
     <div>
-      <h1 className={title()}>About Page</h1>
+      <h1 >About Page</h1>
     </div>
   );
 }
