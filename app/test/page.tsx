374cdfcef4529329de33fea174a5bf39f8b634d9
NutMontree <nutmontree29@gmail.com>
2024-12-24 09:51:05 +0700

    v3

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,6 +4,121 @@ export default function SubTest() {
   return (
     <>
       <div>Sub Test Page</div>
+      <div>
+        <div className="main">
+          <div className="initial-snow">
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+            <div className="snow">&#10052;</div>
+          </div>
+        </div>
+      </div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
+      <div>.</div>
     </>
   );
 }
---
+++ components/snow.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+export default function Snow() {
+  return (
+    <div className="main">
+      <div className="initial-snow">
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+        <div className="snow">&#10052;</div>
+      </div>
+    </div>
+  );
+}
