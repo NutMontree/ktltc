80320f6d73761aa038044eb2578130fa4ef6cca0
NutMontree <nutmontree29@gmail.com>
2024-12-24 09:55:59 +0700

    v3

--- app/showvideos/page.tsx
+++ app/showvideos/page.tsx
@@ -9,6 +9,18 @@ export default function ShowVideos() {
             <iframe
               className="h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 
                        w-[300px] sm:w-[600px] lg:w-[700px] xl:w-[1080px] py-4 rounded-lg"
+              src="https://www.youtube.com/embed/-3MpH0BXQeY"
+              title='การเเข่งขันกีฬาภายใน "ดอกจานเกมส์" ประจำปีการศึกษา 2567 ณ สนามกีฬาวิทยาลัยเทคนิคกันทรลักษ์'
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div>
+            <iframe
+              className="h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 
+                       w-[300px] sm:w-[600px] lg:w-[700px] xl:w-[1080px] py-4 rounded-lg"
               src="https://www.youtube.com/embed/_2Gnilun9X8"
               title="การประกวดสิ่งประดิษฐ์ของคนรุ่นใหม่ ประจำปีการศึกษา 2567"
               frameBorder="0"
