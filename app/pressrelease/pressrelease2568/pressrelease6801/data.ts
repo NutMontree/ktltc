837d40deea8a827b118755a25c32ba00e9857213
NutMontree <nutmontree29@gmail.com>
2025-01-07 16:27:36 +0700

    v3

--- app/pressrelease/pressrelease2568/pressrelease6801/data.ts
+++ app/pressrelease/pressrelease2568/pressrelease6801/data.ts
@@ -1,6 +1,14 @@
 export const DataPressrelease = {
   navItems: [
     {
+      name: `ประกาศเลขที่นั่งสอบ VNET`,
+      backgroundImage: "/images/ข่าวประชาสัมพันธ์/2568/มกราคม/5/1.webp",
+      description: ` 
+ระดับชั้น: ประกาศนียบัตรวิชาชีพ ชั้นปีที่ 3 วิทยาลัยเทคนิคกันทรลักษ์ 19 ม.ค. 2568 13:00 น.
+      ...`,
+      href: "/pressrelease/pressrelease2568/pressrelease6801/pressrelease5",
+    },
+    {
       name: `วิทยาลัยเทคนิคกันทรลักษ์  `,
       backgroundImage: "/images/ข่าวประชาสัมพันธ์/2568/มกราคม/4/2.webp",
       description: ` 
