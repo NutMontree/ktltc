391bbbedf9e3d39f1a45e5d338c24a547cc6a12b
NutMontree <nutmontree29@gmail.com>
2025-01-14 15:55:06 +0700

    v3

--- app/pressrelease/2568/press6801/data.ts
+++ app/pressrelease/2568/press6801/data.ts
@@ -1,7 +1,16 @@
 export const DataPressrelease = {
   navItems: [
     {
-      name: `๑๔ มกราคม  ๒๕๖๘`,
+      name: ` ๑๔ มกราคม  ๒๕๖๘`,
+      backgroundImage: "/images/ข่าวประชาสัมพันธ์/2568/มกราคม/40/1.webp",
+      date: "14 มกราคม 2568",
+      description: ` 
+วิทยาลัยเทคนิคกันทรลักษ์ ขอแสดงความยินดี นางสาวทักษิณา ชมจันทร์ ผู้อำนวยการวิทยาลัยเทคนิคกันทรลักษ์ ได้รับรางวัลเชิดชูครูดีวิถีพุทธ
+      ...`,
+          href: "/pressrelease/2568/press6801/press45", 
+    },
+    {
+      name: ``,
       backgroundImage: "/images/ข่าวประชาสัมพันธ์/2568/มกราคม/39/29.webp",
       date: "14 มกราคม 2568",
       description: ` 
