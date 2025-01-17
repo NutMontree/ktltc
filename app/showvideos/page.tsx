92086dea776e721efbc7b7a7be1f411b96c95bf2
NutMontree <nutmontree29@gmail.com>
2025-01-17 14:11:37 +0700

    Update app\showvideos\page.tsx

--- app/showvideos/page.tsx
+++ app/showvideos/page.tsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function ShowVideos() {
   return (
     <>
-      <div className="flex justify-center">
-        <div className="gap-[24px] grid">
+      <div className="flex justify-center px-[24px]">
+        <div className="gap-[24px] grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 justify-items-center">
           <div className="">
             <iframe
-              className="h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 
-                         w-[300px] sm:w-[600px] lg:w-[700px] xl:w-[1080px]  
+              className="h-[300px] sm:h-[400px] lg:h-[400px] xl:h-[400px] 
+                         w-[350px] sm:w-[600px] lg:w-[500px] xl:w-[400px]  
                          rounded-2xl"
               src="https://www.youtube.com/embed/-3MpH0BXQeY"
               title='การเเข่งขันกีฬาภายใน "ดอกจานเกมส์" ประจำปีการศึกษา 2567 ณ สนามกีฬาวิทยาลัยเทคนิคกันทรลักษ์'
@@ -20,8 +20,8 @@ export default function ShowVideos() {
           </div>
           <div>
             <iframe
-              className="h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 
-                         w-[300px] sm:w-[600px] lg:w-[700px] xl:w-[1080px] 
+              className="h-[300px] sm:h-[400px] lg:h-[400px] xl:h-[400px] 
+                         w-[350px] sm:w-[600px] lg:w-[500px] xl:w-[400px] 
                          rounded-2xl"
               src="https://www.youtube.com/embed/_2Gnilun9X8"
               title="การประกวดสิ่งประดิษฐ์ของคนรุ่นใหม่ ประจำปีการศึกษา 2567"
@@ -33,8 +33,8 @@ export default function ShowVideos() {
           </div>
           <div>
             <iframe
-              className="h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 
-                         w-[300px] sm:w-[600px] lg:w-[700px] xl:w-[1080px] 
+              className="h-[300px] sm:h-[400px] lg:h-[400px] xl:h-[400px] 
+                         w-[350px] sm:w-[600px] lg:w-[500px] xl:w-[400px] 
                          rounded-2xl"
               src="https://www.youtube.com/embed/1qwOVzMyCQU"
               title="กิจกรรมโครงการเดิน วิ่ง ปั่น ป้องกันอัมพาต ครั้งที่ 10 วิทยาลัยเทคนิคกันทรลักษ์ KTLTC"
