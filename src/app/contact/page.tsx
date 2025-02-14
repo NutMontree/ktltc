64f532102a13f525de43056f33634b3c045be091
NutMontree <nutmontree29@gmail.com>
2025-02-14 21:00:38 +0700

    'v4'

--- src/app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import { title } from "@/components/primitives";
-
-export default function ContactPage() {
-  return (
-    <div>
-      <h1 className={title()}>Contact Page</h1>
-    </div>
-  );
-}
