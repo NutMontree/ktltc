3c6027ff50fcb16394a226d91eb8d0f979dac289
NutMontree <nutmontree29@gmail.com>
2025-02-24 12:55:19 +0700

    v3

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -2,6 +2,7 @@
 
 import { Accordion, AccordionItem } from "@nextui-org/react";
 import Planning from "./Planning";
+import Link from "next/link";
 
 export default function Resource() {
   return (
@@ -24,8 +25,13 @@ export default function Resource() {
           >
             <Planning />
           </AccordionItem>
-          <AccordionItem key="2" aria-label="Accordion 2" title="ไม่มีข้อมูล">
+          <AccordionItem key="2" aria-label="Accordion 2" title="งานแผน">
             {/* <Pressrelease2566 /> */}
+            <Link href='https://ktltcp.vercel.app/' target="_blank">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                Ktltc Plan
+              </button>
+            </Link>
           </AccordionItem>
         </Accordion>
       </div>
