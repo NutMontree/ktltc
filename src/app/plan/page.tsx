3243a73d1229ceef462a3a3c93c2074e37f4742b
NutMontree <nutmontree29@gmail.com>
2025-02-25 17:31:45 +0700

    v4

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -3,6 +3,10 @@
 import { Accordion, AccordionItem } from "@nextui-org/react";
 import Planning from "./Planning";
 import Link from "next/link";
+import PAB from "./PAB";
+import CW from "./CW";
+import TABPW from "./TABPW";
+import RDIAI from "./RDIAI";
 
 export default function Resource() {
   return (
@@ -20,18 +24,27 @@ export default function Resource() {
         <Accordion isCompact>
           <AccordionItem
             key="1"
-            aria-label="Accordion 1"
+            aria-label="1"
             title="แผนภูมิโครงสร้างการบริหารงานสถานศึกษา"
           >
             <Planning />
           </AccordionItem>
-          <AccordionItem key="2" aria-label="Accordion 2" title="งานแผน">
-            {/* <Pressrelease2566 /> */}
+          <AccordionItem key="2" aria-label="2" title="งานวางแผน และงบประมาณ">
+            <PAB />
             <Link href='https://ktltcp.vercel.app/' target="_blank">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full">
                 Ktltc Plan
               </button>
             </Link>
+          </AccordionItem>
+          <AccordionItem key="3" aria-label="3" title="งานความร่วมมือ">
+            <CW />
+          </AccordionItem>
+          <AccordionItem key="4" aria-label="4" title="งานส่งเสริมผลิตผล การค้าและประกอบธุรกิจ">
+            <TABPW />
+          </AccordionItem>
+          <AccordionItem key="5" aria-label="5" title="งานวิจัย พัฒนา นวัตกรรมและสิ่งประดิษฐ์">
+            <RDIAI />
           </AccordionItem>
         </Accordion>
       </div>
