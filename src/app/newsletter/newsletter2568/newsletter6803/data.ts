c7ba4cd05494ceaea4c7d2cd4331e760d6c8b00a
NutMontree <nutmontree29@gmail.com>
2025-03-11 14:33:15 +0700

    v4

--- src/app/newsletter/newsletter2568/newsletter6803/data.ts
+++ src/app/newsletter/newsletter2568/newsletter6803/data.ts
@@ -1,6 +1,15 @@
 export const Data = {
   navItems: [
     {
+      name: `๑๐ มีนาคม ๒๕๖๘`,
+      backgroundImage: "/images/ข่าวประชาสัมพันธ์/2568/มีนาคม/6/0.webp",
+      date: "7 มีนาคม 2568",
+      description: ` 
+นางสาวทักษิณา ชมจันทร์ ผู้อำนวยการวิทยาลัยเทคนิคกันทรลักษ์ จัดโครงการสัมมนาการจัด
+        ...`,
+      href: "/pressrelease/2568/press6803/press10",
+    },
+    {
       name: `๒ มีนาคม ๒๕๖๘ `,
       backgroundImage: "/images/ข่าวประชาสัมพันธ์/2568/กุมภาพันธ์/66/0.webp",
       date: "3 มีนาคม 2568",
