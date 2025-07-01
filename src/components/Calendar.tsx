4a69c2c5902ede02a9a66a6fee680df9343939da
NutMontree <nutmontree29@gmail.com>
2025-07-01 11:57:20 +0700

    v1

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,17 +1,25 @@
-"use client"
-import { Calendar } from "@heroui/calendar";
-import { today, getLocalTimeZone } from "@internationalized/date";
+// "use client"
+// import { Calendar } from "@heroui/calendar";
+// import { today, getLocalTimeZone } from "@internationalized/date";
 
-export default function CalendarPage() {
+// export default function CalendarPage() {
+//     return (
+//         <>
+//             <div className="flex justify-center">
+//                 <Calendar
+//                     isReadOnly
+//                     aria-label="Date (Read Only)"
+//                     value={today(getLocalTimeZone())}
+//                 />
+//             </div>
+//         </>
+//     )
+// }
+
+import React from 'react'
+
+export default function Calendar() {
     return (
-        <>
-            <div className="flex justify-center">
-                <Calendar
-                    isReadOnly
-                    aria-label="Date (Read Only)"
-                    value={today(getLocalTimeZone())}
-                />
-            </div>
-        </>
+        <div>Calendar</div>
     )
 }
