6840e555f408048a50d6e1d4ed18f6fdb4d02f67
NutMontree <nutmontree29@gmail.com>
2025-07-01 13:06:38 +0700

    v1

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,16 +1,27 @@
 "use client"
-import { Calendar } from "@heroui/calendar";
-import { today, getLocalTimeZone } from "@internationalized/date";
+import React from 'react';
+import { Calendar, theme } from 'antd';
+import type { CalendarProps } from 'antd';
+import type { Dayjs } from 'dayjs';
+
+const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
+    console.log(value.format('YYYY-MM-DD'), mode);
+};
 
 export default function CalendarPage() {
+    const { token } = theme.useToken();
+
+    const wrapperStyle: React.CSSProperties = {
+        width: 300,
+        border: `1px solid ${token.colorBorderSecondary}`,
+        borderRadius: token.borderRadiusLG,
+    };
     return (
         <>
             <div className="flex justify-center">
-                <Calendar
-                    isReadOnly
-                    aria-label="Date (Read Only)"
-                    value={today(getLocalTimeZone())}
-                />
+                <div style={wrapperStyle}>
+                    <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+                </div>
             </div>
         </>
     )
