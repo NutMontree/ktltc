1be26725d04d05b5b0348ed0fa37e57652f5dc00
NutMontree <nutmontree29@gmail.com>
2025-09-22 15:40:01 +0700

    v1

--- src/app/internships/SubInternships/2568/data.ts
+++ src/app/internships/SubInternships/2568/data.ts
@@ -2,6 +2,14 @@ export const DataInternships = {
   navItems: [
     {
       name: `นักศึกษา`,
+      backgroundImage: "/images/internships/2568/24/2.webp",
+      date: "22 กันยายน 2568",
+      description: `
+      วิทยาลัยเทคนิคกันทรลักษ์ รายงานการฝึกประสบการณ์วิชาชีพ`,
+      href: "/internships/SubInternships/2568/24",
+    },
+    {
+      name: `นักศึกษา`,
       backgroundImage: "/images/internships/2568/23/1.webp",
       date: "16 กันยายน 2568",
       description: `
