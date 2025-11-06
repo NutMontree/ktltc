95bd297e367c69993b7dcc2525434be4b3c386e1
NutMontree <nutmontree29@gmail.com>
2025-11-06 08:37:10 +0700

    6/11/68

---
+++ src/app/pressrelease/2568/press6811/press5/data.ts
@@ -0,0 +1,85 @@
+// ชื่อข่าว 
+export const DataPressrelease = {
+  Item: [
+    { title: `ข่าวประชาสัมพันธ์`, },
+    { title: `วิทยาลัยเทคนิคกันทรลักษ์`, },
+  ],
+};
+
+// วันที่ลงข้อมูล 
+export const DataDate = [{ date: `6 พฤศจิกายน 2568` }];
+
+// คำอธิบายข่าว
+export const Description = [
+  { description: `5 พฤศจิกายน 2568 ` },
+  { description: `      วิทยาลัยเทคนิคกันทรลักษ์ ร่วมสืบสานประเพณีลอยกระทง ประจำปี 2568` },
+  { description: `      ขอเชิญปนักเรียน นักศึกษา มาร่วมสืบสานประเพณีไทยในวันลอยกระทง ร่วมกันลอยกระทงเพื่อขอขมาพระแม่คงคา และร่วมอนุรักษ์วัฒนธรรมอันงดงามของไทย ` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+];
+
+// ลิงค์
+export const TageLink = [
+  {
+    tage: ``,
+    href: ``,
+  },
+
+  // {
+  // tage: ``,
+  // href: ``,
+  // },
+]
+
+// รูปภาพ
+export const ImageItem = [
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/01.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/02.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/03.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/05.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/06.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/06.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/07.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/08.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/09.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/10.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/11.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/12.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/13.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/15.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/15.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/16.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/17.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/5/18.webp" },
+]
