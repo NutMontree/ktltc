8ee72dfcf021a1b900b0f99e00356578b079e3d6
NutMontree <nutmontree29@gmail.com>
2025-11-07 19:49:40 +0700

    7/11/68

---
+++ src/app/pressrelease/2568/press6811/press10/data.ts
@@ -0,0 +1,61 @@
+// ชื่อข่าว 
+export const DataPressrelease = {
+  Item: [
+    { title: `ประกาศ`, },
+    { title: `วิทยาลัยเทคนิคกันทรลักษ์`, },
+  ],
+};
+
+// วันที่ลงข้อมูล 
+export const DataDate = [{ date: `7 พฤศจิกายน 2568` }];
+
+// คำอธิบายข่าว
+export const Description = [
+  { description: `วันที่ 6 พฤศจิกายน 2568` },
+  { description: `       นางสาวทักษิณา ชมจันทร์ ผู้อำนวยการวิทยาลัยเทคนิคกันทรลักษ์ เข้าร่วมประชุมผู้บริหารสำนักงานคณะกรรมการการอาชีวศึกษา ครั้งที่ 6 ปีการศึกษา 2568 ผ่านระบบออนไลน์ การประชุมครั้งนี้จัดขึ้นเพื่อรับทราบนโยบาย แนวทางการดำเนินงาน และการบริหารจัดการด้านการอาชีวศึกษา พร้อมทั้งเป็นการติดตามผลการดำเนินงานตามภารกิจที่เกี่ยวข้องของสถานศึกษาอย่างเป็นระบบ ณ ห้องประชุมผู้อำนวยการ วิทยาลัยเทคนิคกันทรลักษ์` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+];
+
+// ลิงค์
+export const TageLink = [
+  {
+    tage: ``,
+    href: ``,
+  },
+  // {
+  // tage: ``,
+  // href: ``,
+  // },
+]
+
+// รูปภาพ
+export const ImageItem = [
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/10/1.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/10/2.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/10/4.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/10/6.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/10/7.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/10/00.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/10/0.webp" },
+
+]
---
+++ src/app/pressrelease/2568/press6811/press8/data.ts
@@ -0,0 +1,91 @@
+// ชื่อข่าว 
+export const DataPressrelease = {
+  Item: [
+    { title: `ประกาศ`, },
+    { title: `วิทยาลัยเทคนิคกันทรลักษ์`, },
+  ],
+};
+
+// วันที่ลงข้อมูล 
+export const DataDate = [{ date: `7 พฤศจิกายน 2568` }];
+
+// คำอธิบายข่าว
+export const Description = [
+  { description: ` นางสาวทักษิณา ชมจันทร์ ผู้อำนวยการวิทยาลัยเทคนิคกันทรลักษ์ มอบหมายให้ นายอาทร ศรีมะณี รองผู้อำนวยการฝ่ายวิชาการ พร้อมด้วยคณะครู และบุคลากรทางการศึกษา เข้าร่วมพิธีบำเพ็ญกุศลสวดพระอภิธรรมอุทิศถวายพระราชกุศล สมเด็จพระนางเจ้าสิริกิติ์ พระบรมราชินีนาถ พระบรมราชชนนีพันปีหลวง` },
+  { description: `        ซึ่งจัดพิธีบำเพ็ญกุศลตั้งแต่วันที่ 28 ตุลาคม – 25 พฤศจิกายน 2568 ณ ศาลาการเปรียญวัดศิริวราวาส อำเภอกันทรลักษ์ จังหวัดศรีสะเกษ` },
+  { description: `ธ สถิตในดวงใจไทยนิรันดร์` },
+  { description: `       ด้วยเกล้าด้วยกระหม่อมขอเดชะ ข้าพระพุทธเจ้า` },
+  { description: ` คณะผู้บริหาร ครู บุคลากรทางการศึกษา เเละนักเรียน นักศึกษา` },
+  { description: `วิทยาลัยเทคนิคกันทรลักษ์` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+];
+
+// ลิงค์
+export const TageLink = [
+
+  {
+    tage: `รูปภาพเพิ่มเติม https://photos.app.goo.gl/RJXkyvwGWnCC5LV8A`,
+    href: `https://photos.google.com/share/AF1QipO-gL8e2VyGigPp6uhi1x4E7QYoFzdFLvN3VAR1eOW7u0vb8zaEjHwVMJel_Em2Sw?key=bDhZcWh2ZkNmMXp1UVd2X2hzdGFaM29jbEljbFlB`,
+  },
+  // {
+  // tage: ``,
+  // href: ``,
+  // },
+]
+
+// รูปภาพ
+export const ImageItem = [
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/01.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/02.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/03.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/04.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/05.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/06.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/07.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/08.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/09.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/10.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/11.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/12.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/13.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/14.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/15.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/16.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/17.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/18.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/19.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/20.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/21.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/22.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/23.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/24.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/25.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/00.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/8/0.webp" },
+
+]
