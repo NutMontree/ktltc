6750e3c50878d3946d564086571810dd044c20a4
NutMontree <nutmontree29@gmail.com>
2025-11-11 17:02:13 +0700

    11/11/68

---
+++ src/app/pressrelease/2568/press6811/press14/data.ts
@@ -0,0 +1,41 @@
+// ชื่อข่าว 
+export const DataPressrelease = {
+  Item: [
+    { title: `ข่าวประชาสัมพันธ์`, },
+    { title: `วิทยาลัยเทคนิคกันทรลักษ์`, },
+  ],
+};
+
+// วันที่ลงข้อมูล 
+export const DataDate = [{ date: `11 พฤศจิกายน 2568` }];
+
+// คำอธิบายข่าว
+export const Description = [
+  { description: `วิทยาลัยเทคนิคกันทรลักษ์ ` },
+  { description: `    ร่วมส่งกำลังใจ กองทัพทหารไทย ที่ได้รับบาดเจ็บ เเละรับผลกระทบทุกนาย วันที่ 10 พฤศจิกายน 2568 ` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+];
+
+// ลิงค์
+export const TageLink = [
+  {
+    tage: ``,
+    href: ``,
+  },
+  // {
+  // tage: ``,
+  // href: ``,
+  // },
+]
+
+// รูปภาพ
+export const ImageItem = [
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/14/00.webp" },
+]
