3b598f247109c3acb8ef3a8792300a07cd5a73a2
NutMontree <nutmontree29@gmail.com>
2025-11-12 14:32:11 +0700

    Update head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,12 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>
-        ktltc
-      </title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="ktltc" content="วิทยาลัยเทคนิคกันทรลักษ์" />
-      <link rel="icon" href="/images/logo.icofavicon.ico" />
+
     </>
   );
 }
