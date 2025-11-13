e2b4c88b24c2f95a90d06835b6d1ceb698d1492d
NutMontree <nutmontree29@gmail.com>
2025-11-13 11:56:58 +0700

    13/11/68

---
+++ src/app/pressrelease/2568/press6811/press30/data.ts
@@ -0,0 +1,61 @@
+// ชื่อข่าว 
+export const DataPressrelease = {
+  Item: [
+    { title: `ข่าวประชาสัมพันธ์`, },
+    { title: `วิทยาลัยเทคนิคกันทรลักษ์`, },
+  ],
+};
+
+// วันที่ลงข้อมูล 
+export const DataDate = [{ date: `13 พฤศจิกายน 2568` }];
+
+// คำอธิบายข่าว
+export const Description = [
+  { description: `วันที่ 12 พฤศจิกายน 2568  –  โดย นางสาวทักษิณา ชมจันทร์ ผู้อำนวยการวิทยาลัยเทคนิคกันทรลักษ์ พร้อมด้วยคณะครู นำนักเรียน นักศึกษาชมรมจิตอาสากู้ภัยเทคนิคกันท์ ร่วมบำเพ็ญประโยชน์เก็บขยะในชุมชน บริเวณหมู่บ้านโดยรอบวิทยาลัยเทคนิคกันทรลักษ์ ภายใต้งานกองทุนเพื่อความเสมอภาคทางการศึกษา` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+];
+
+// ลิงค์
+export const TageLink = [
+  {
+    tage: ``,
+    href: ``,
+  },
+  // {
+  // tage: ``,
+  // href: ``,
+  // },
+]
+
+// รูปภาพ
+export const ImageItem = [
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/30/1.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/30/2.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/30/5.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/30/7.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/30/8.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/30/3.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/30/6.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/30/4.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/30/9.webp" },
+]
