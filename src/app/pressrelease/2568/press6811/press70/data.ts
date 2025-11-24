c56b628b29674d45dcf6d1e5f7686b5d162ebe9e
NutMontree <nutmontree29@gmail.com>
2025-11-24 09:04:06 +0700

    24/11/2568

---
+++ src/app/pressrelease/2568/press6811/press70/data.ts
@@ -0,0 +1,69 @@
+// ชื่อข่าว 
+export const DataPressrelease = {
+  Item: [
+    { title: `ข่าวประชาสัมพันธ์`, },
+    { title: `วิทยาลัยเทคนิคกันทรลักษ์`, },
+  ],
+};
+
+// วันที่ลงข้อมูล 
+export const DataDate = [{ date: `24 พฤศจิกายน 2568` }];
+
+// คำอธิบายข่าว
+export const Description = [
+  { description: `วันที่ 19 พฤศจิกายน 2568 ` },
+  { description: `        นางสาวทักษิณา ชมจันทร์ ผู้อำนวยการวิทยาลัยเทคนิคกันทรลักษ์  มอบหมายให้ นายสมศักดิ์ จันทานิตย์ รองผู้อำนวยการฝ่ายเเผนงานเเละความร่วมมือ พร้อมด้วย นายอดุลย์ ทวินันท์ หัวหน้างานวิจัยพัฒนานวัตกรรมและสิ่งประดิษฐ์ คณะครู` },
+  { description: `        ร่วมประชุมคณะกรรมการดำเนินงานการจัดการประกวดสิ่งประดิษฐ์ของคนรุ่นใหม่ ระดับสถานศึกษาจังหวัดศรีสะเกษ ประจำปีการศึกษา 2568  ณ ห้องประชุมชั้น 3 อาคารอำนวยการวิทยาลัยเทคนิคกันทรลักษ์  ` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+];
+
+// ลิงค์
+export const TageLink = [
+  {
+    tage: ``,
+    href: ``,
+  },
+  // {
+  // tage: ``,_
+  // href: ``,_
+  // },_
+]
+
+// รูปภาพ_
+export const ImageItem = [
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/70/7.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/70/1.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/70/2.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/70/3.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/70/5.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/70/6.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/70/4.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/70/00.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/พฤศจิกายน/70/0.webp" },
+]
