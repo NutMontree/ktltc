771f57b9046c9295fb1fa7aa266e3ea5755ce357
NutMontree <nutmontree29@gmail.com>
2025-12-15 07:43:14 +0700

    15/12/68

---
+++ src/app/pressrelease/2568/press6812/press26/data.ts
@@ -0,0 +1,58 @@
+// ชื่อข่าว
+export const DataPressrelease = {
+  Item: [{ title: `ข่าวประชาสัมพันธ์` }, { title: `วิทยาลัยเทคนิคกันทรลักษ์` }],
+};
+
+// วันที่ลงข้อมูล
+export const DataDate = [{ date: `15 ธันวาคม 2568` }];
+
+// คำอธิบายข่าว
+export const Description = [
+  { description: `วิทยาลัยเทคนิคกันทรลักษ์ ` },
+  {
+    description: `      นำโดย นางสาวทักษิณา ชมจันทร์ ผู้อำนวยการวิทยาลัยเทคนิคกันทรลักษ์ พร้อมด้วยคณะผู้บริหาร คณะครู และบุคลากรทางการศึกษา นักเรียน นักศึกษา `,
+  },
+  { description: `ขอสดุดีทหารกล้า พลีชีพนี้เพื่อเเผนดินไทย` },
+  { description: `“เราจะจดจำความดีที่คุณได้ทำเพื่อประเทศของเราเสมอ”` },
+  {
+    description: `      การเสียสละชีวิตเพื่อปกป้องเอกราชอธิปไตยของชาติจากศัตรูผู้รุกราน คือเกียรติสูงสุดที่ควรพึงได้รับ`,
+  },
+  {
+    description: `We salute the brave soldiers who sacrificed their lives for Thailand.`,
+  },
+  {
+    description: `"We will always remember the good you have done for our country."`,
+  },
+  {
+    description: `        The sacrifice of one's life to protect the nation's independence and sovereignty from invading enemies is the highest honor deserved.`,
+  },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+];
+
+// ลิงค์
+export const TageLink = [
+  {
+    tage: ``,
+    href: ``,
+  },
+  // {
+  // tage: ``,
+  // href: ``,
+  // },
+];
+// รูปภาพ
+export const ImageItem = [
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/ธันวาคม/26/1.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2568/ธันวาคม/26/2.webp" },
+];
