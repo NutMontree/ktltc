c36a9adfbd6f2f6f6fd6932a5cb62336506bbaa5
NutMontree <nutmontree29@gmail.com>
2025-12-16 18:25:50 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,16 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // 1. ปิดการตรวจสอบ TypeScript ในระหว่าง Build/Dev
-    typescript: {
-        // ⚠️ อนุญาตให้ Build ผ่านไปได้ แม้จะมี Type Error ภายใน (แก้ปัญหา Promise/params)
-        ignoreBuildErrors: true,
-    },
-    // บล็อก 'eslint' ถูกลบออกแล้ว
+  // 1. ปิดการตรวจสอบ TypeScript ในระหว่าง Build/Dev
+  typescript: {
+    // ⚠️ อนุญาตให้ Build ผ่านไปได้ แม้จะมี Type Error ภายใน (แก้ปัญหา Promise/params)
+    ignoreBuildErrors: true,
+  },
+  // บล็อก 'eslint' ถูกลบออกแล้ว
+  compress: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 module.exports = nextConfig;
