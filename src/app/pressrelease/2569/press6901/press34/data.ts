e2962ec1f9fdb166a5aa5ac24e83bb343f5b4abb
NutMontree <nutmontree29@gmail.com>
2026-01-15 20:34:09 +0700

    v1

---
+++ src/app/pressrelease/2569/press6901/press34/data.ts
@@ -0,0 +1,66 @@
+// ชื่อข่าว
+export const DataPressrelease = {
+  Item: [{ title: `ข่าวประชาสัมพันธ์` }, { title: `วิทยาลัยเทคนิคกันทรลักษ์` }],
+  Pagination: {
+    prev: {
+      href: "/pressrelease/2569/press6901/press33",
+    },
+    next: {
+      href: "/pressrelease/2569/press6901/press35",
+    },
+    // หมายเหตุ: หากไม่มีเรื่องก่อนหน้าหรือถัดไป ให้ใส่ค่าเป็น null เช่น prev: null
+  },
+};
+
+// วันที่ลงข้อมูล
+export const DataDate = [{ date: `15 มกราคม 2569` }];
+
+// คำอธิบายข่าว
+export const Description = [
+  { description: `วันที่ 15 มกราคม 2569` },
+  {
+    description: `     นางสาวทักษิณา ชมจันทร์ ผู้อำนวยการวิทยาลัยเทคนิคกันทรลักษ์ พร้อมด้วยคณะผู้บริหาร ครู และบุคลากรทางการศึกษา ร่วมเป็นเจ้าภาพทำบุญสร้างโรงครัว ณ วัดบ้านโนนกางของ (บ้านทุ่งจาน) ตำบลจานใหญ่ อำเภอกันทรลักษ์ จังหวัดศรีสะเกษ`,
+  },
+  {
+    description: `     ทั้งนี้ เพื่อร่วมสืบสานประเพณีอันดีงามทางพระพุทธศาสนา ส่งเสริมความสามัคคี และเป็นการสนับสนุนกิจกรรมของวัดให้เกิดประโยชน์ต่อพระภิกษุสามเณรและชุมชนโดยรอบ`,
+  },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+];
+
+// ลิงค์
+export const TageLink = [
+  // {
+  //   tage: ``,
+  //   href: ``,
+  // },
+];
+// รูปภาพ
+export const ImageItem = [
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/34/1.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/34/2.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/34/3.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/34/4.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/34/5.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/34/6.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/34/7.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/34/8.webp" },
+];
+
+export const ZeroNameImage = [
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/34/00.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/34/0.webp" },
+];
---
+++ src/app/pressrelease/2569/press6901/press35/data.ts
@@ -0,0 +1,59 @@
+// ชื่อข่าว
+export const DataPressrelease = {
+  Item: [{ title: `ข่าวประชาสัมพันธ์` }, { title: `วิทยาลัยเทคนิคกันทรลักษ์` }],
+  Pagination: {
+    prev: {
+      href: "/pressrelease/2569/press6901/press34",
+    },
+    next: {
+      href: "/pressrelease/2569/press6901/press36",
+    },
+    // หมายเหตุ: หากไม่มีเรื่องก่อนหน้าหรือถัดไป ให้ใส่ค่าเป็น null เช่น prev: null
+  },
+};
+
+// วันที่ลงข้อมูล
+export const DataDate = [{ date: `15 มกราคม 2569` }];
+
+// คำอธิบายข่าว
+export const Description = [
+  { description: `วันที่ 15 มกราคม 2569 ` },
+  {
+    description: `    นางสาวทักษิณา ชมจันทร์ ผู้อำนวยการวิทยาลัยเทคนิคกันทรลักษ์ มอบหมายให้ ผู้กำกับลูกเสือและลูกเสือเนตรนารีวิสามัญ เข้าร่วมประเมินคุณภาพงานลูกเสือเนตรนารีวิสามัญ ระดับจังหวัด ณ วิทยาลัยการอาชีพขุนหาญ`,
+  },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+  // { description: `` },
+];
+
+// ลิงค์
+export const TageLink = [
+  // {
+  //   tage: ``,
+  //   href: ``,
+  // },
+];
+// รูปภาพ
+export const ImageItem = [
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/35/1.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/35/2.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/35/3.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/35/4.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/35/5.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/35/6.webp" },
+];
+
+export const ZeroNameImage = [
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/35/00.webp" },
+  { imgs: "/images/ข่าวประชาสัมพันธ์/2569/มกราคม/35/0.webp" },
+];
