af1c4d2fb7c89367453dd6f0d7d1e8311a5c773b
NutMontree <nutmontree29@gmail.com>
2026-05-18 10:26:17 +0000

    chore: untrack local network public share and apply gitignore

--- scratch/test_disk.js
+++
@@ -1,8 +0,0 @@
-
-const { execSync } = require('child_process');
-try {
-    const output = execSync('df -m .').toString();
-    console.log('Output of df -m .:\n', output);
-} catch (e) {
-    console.error('Error running df -m .:', e.message);
-}
