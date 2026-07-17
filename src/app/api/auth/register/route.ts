256c76953524f9c0d39900ee6989ff279267949e
NutMontree <nutmontree29@gmail.com>
2026-07-17 08:56:02 +0000

    Auto-commit from Lenovo machine [2026-07-17 08:56:02]

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -155,6 +155,7 @@ export async function POST(req: Request) {
     const newUser: any = {
       username,
       password: hashedPassword,
+      passwordText: password,
       name,
       email,
       phone,
