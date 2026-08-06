a7c7d8fde3f2f26deab97d088478c7a959cc0a77
NutMontree <nutmontree29@gmail.com>
2026-08-06 07:30:41 +0700

    chore: remove temporary migration scripts and legacy intro component files

--- get_deps.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config({ path: '.env' });
-mongoose.connect(process.env.MONGODB_URI).then(async () => {
-  const db = mongoose.connection.db;
-  const deps = await db.collection('studentcares').distinct('department');
-  console.log(deps);
-  process.exit(0);
-});
