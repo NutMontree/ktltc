f14a77babdfc094c60e8be719f3e2535d11afeaf
NutMontree <nutmontree29@gmail.com>
2026-08-07 03:34:02 +0000

    Auto-commit from Lenovo machine [2026-08-07 03:34:02]

--- scripts/migrate-all-menus.ts
+++ scripts/migrate-all-menus.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb://nut:Nut29122539@100.64.196.104:27017/ktltc_db?authSource=admin";
+const uri = "mongodb://nut:Nut29122539@127.0.0.1:27017/ktltc_db?authSource=admin";
 const client = new MongoClient(uri);
 
 async function run() {
